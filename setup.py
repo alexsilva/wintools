3523cd6048348d744cfdb45772946c6c6720c592
alex <geniofuturo@gmail.com>
2020-11-19 16:07:17 -0300

    Add setup.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wintools',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/alexsilva/wintools',
+    license='MIT',
+    author='alex',
+    author_email='alex@fabricadigital.com.br',
+    description='Windows processing tools'
+)
